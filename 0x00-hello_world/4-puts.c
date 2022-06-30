efa6abbd8532d31875064e18b7e558da6bc9399b
Pkal99 <kaleabay99@gmail.com>
2022-06-30 06:51:37 -0700

    prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/* print text  */
+/* print text in the main */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
