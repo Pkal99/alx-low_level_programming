f369439e8b495c77b0b81ce68ed25c63958dc4ae
Pkal99 <kaleabay99@gmail.com>
2022-07-07 01:05:14 -0700

    function that prints every minute of the day of Jack Bauer,

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* Print every minute of the day of Jack Bauer
+* jack_bauer -Print every minute of the day of Jack Bauer
 * Description: Start from 00:00 to 23:59
 */
 void jack_bauer(void)
