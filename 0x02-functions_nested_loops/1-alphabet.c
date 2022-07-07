b41246af7e0f7b75d7e19f2f2c48694094ed47e6
Pkal99 <kaleabay99@gmail.com>
2022-07-06 22:59:22 -0700

    prints the alphabet, in lowercase,

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *main -  print_alphabet Print alphabet followed by a new line.
- *Description: You are not allowed to include standard libraries.
- *Return: 0
+ * print_alphabet Print alphabet followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
 void print_alphabet(void)
 {
