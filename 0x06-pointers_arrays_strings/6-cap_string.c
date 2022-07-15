7e1d9ed0d5274b6f028082acdd877ec0caea3ae4
Pkal99 <kaleabay99@gmail.com>
2022-07-15 05:33:01 -0700

    a function that capitalizes all words of a string. and update main.h

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* cap_string - capitalize all words of a string
+* @str: string
+* Separators of words: space, tabulation, new line, ,, ;, ., !, ?, , (, ), {, and }
+* Return: `string`
+*/
+char *cap_string(char *str)
+{
+	int i, a;
+	int chg;
+	char notations[] = ",;.!?(){}\n\t\" ";
+
+	for (i = 0, chg = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] > 96 && str[0] < 123)
+			chg = 1;
+		for (c = 0; notations[a] != '\0'; c++)
+		{
+			if (notations[a] == str[i])
+				chg = 1;
+		}
+
+		if (chg)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				chg = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				chg = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				chg = 0;
+		}
+	}
+	return (str);
+}
