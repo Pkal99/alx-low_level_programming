9a108b696e8cbf93d1f0215deafc1d2730473a59
Pkal99 <kaleabay99@gmail.com>
2022-07-25 03:12:15 -0700

    function that locates a character in a string and update main.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strchr - locate character in a string
+* @s: char array of a string
+* @c: char to look for char from string
+* Return: NULL if char not found, or pointer to first occurrence of char `c`
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
+	
+	return (s + 1);
+}
