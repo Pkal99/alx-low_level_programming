ff16c41b2d00bc1a5333ef9201fdbdcd57fd5161
Pkal99 <kaleabay99@gmail.com>
2022-07-26 05:28:03 -0700

    update on a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,45 +1,31 @@
 #include "main.h"
 /**
-* is_prime_number - determine if a number is a prime number
-* @n: int number
-* Return: 1 if prime, 0 otherwise
+* prime_n - determine if n is a prime
+* @n: test case
+* @i: recurring factors
+* Description: determines if n is a prime using 2 parameters
+* Return: 1 if n is a prime, 0 if not
+*/
+int prime_n(int n, int i)
+{
+	if (n % i == 0)
+		return (0);
+
+	else if (n >= (i + 1) * (i * 1))
+		return (prime_n(n, i + 1));
+
+	return (1);
+}
+/**
+* is_prime_number - determine if n is a prime
+* @n: number to check
+* Description: determine if n is a prime
+* Return: 1 if prime, 0 if not
 */
 int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
-	if (n < 4)
-		return (1);
-	return (hai(n, 2));
-}
-/**
-* _sqrt - return square root of number
-* @x: number
-* @i: number incrementer acting as divisor
-* Return: square root of `x`
-*/
-int _sqrt(int x, int i)
-{
-	int square;
 
-	square = i * i;
-	if (square >= x)
-		return (i);
-	else
-		return (_sqrt(x, i + 1));
-}
-/**
-* hai - helper function, recursive steps taken
-* @n: number given to original function is_prime_number
-* @d: incrementer divisor
-* Return: 0 if not prime, 1 if prime
-*/
-int hai(int n, int d)
-{
-	if (n % d == 0)
-		return (0);
-	else if (_sqrt(n, 1) < d)
-		return (1);
-	else
-		return (hai(n, d + 1));
+	return (prime_n(n, 2));
 }
