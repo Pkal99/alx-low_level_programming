6b969b6f30479e38d8b40e885426daf37d83ae71
Pkal99 <kaleabay99@gmail.com>
2022-07-28 03:53:24 -0700

    update on function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 /**
 * _strchr - locate character in a string
 * @s: char array of a string
-* @c: char to look for char from string
+* @c: char to look for char from string s
 * Return: NULL if char not found, or pointer to first occurrence of char `c`
 */
 char *_strchr(char *s, char c)
