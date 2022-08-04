6babd3394507c71ab44b2483e7dd6b73f68002dd
Pkal99 <kaleabay99@gmail.com>
2022-08-04 01:29:40 -0700

    a function that prints numbers, followed by a new line.

--- 0x10-variadic_functions/main.h
+++ 0x10-variadic_functions/main.h
@@ -41,3 +41,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
