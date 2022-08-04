1a2dbf155b2728296cfe041deb6db86ddef32744
Pkal99 <kaleabay99@gmail.com>
2022-08-04 01:59:54 -0700

    a function that prints anything. and updated main.h

--- 0x10-variadic_functions/main.h
+++ 0x10-variadic_functions/main.h
@@ -43,3 +43,4 @@ char *leet(char *);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+oid print_all(const char * const format, ...);
