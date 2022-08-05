1f89a423e3e394f14329f885533d1a071370cff2
Pkal99 <kaleabay99@gmail.com>
2022-08-05 05:30:42 -0700

    update on  a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_name - Print a name from a function pointer
-* @name: char string
+* @name: char string name
 * @f: function pointer that takes a string argument
 */
 void print_name(char *name, void (*f)(char *))
