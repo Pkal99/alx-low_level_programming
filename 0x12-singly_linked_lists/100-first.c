c3481262fb322352418bbe180bc66e16ea9db2b3
Pkal99 <kaleabay99@gmail.com>
2022-08-13 06:16:55 -0700

    print string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* before_main - Print statement within this fxn before running main fun
+* Return: Nothing.
+*/
+void first(void) __attribute__ ((constructor));
+
+void first(void)
+{
+		printf("You're beat! and yet, you must allow,\n");
+			printf("I bore my house upon my bac
